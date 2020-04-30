6f5e58c3f780bbd7417b85a86585cfb98e3c478d
Daniel <elrion018@gmail.com>
2020-05-01 00:21:26 +0900

    20200501

--- todoList_server/account/models.py
+++ todoList_server/account/models.py
@@ -6,7 +6,7 @@ class Account(models.Model):
     password = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    push_token = models.CharField(max_length=200)
+    push_token = models.CharField(max_length=200, blank=False, null=True)
 
     class Meta:
         db_table = "account"
